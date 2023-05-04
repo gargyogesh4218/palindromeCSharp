144d09b1d1989916e6868ac900150bdc5ed28d00
gargyogesh4218 <125199173+gargyogesh4218@users.noreply.github.com>
2023-05-04 12:38:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        primeOrNot();
+
+        Console.WriteLine("enter a number which to be check if it is palindrome or not :");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int a = n;
+
+        int m = 0;
+        while (n > 0)
+        {
+            int r = n % 10;
+            m = 10 * m + r;
+            n = n / 10;
+        }
+
+        if (a == m)
+            Console.WriteLine("palindrome");
+        else
+            Console.WriteLine("not palindrome");
+
+        Console.ReadKey();
+    }
+}
